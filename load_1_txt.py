1f38b9b9947f89dbfc864353590f0b969c9bcb77
matthew Famakin <matthewadesida@gmail.com>
2019-04-28 05:44:23 +0100

    Single Text File

---
+++ load_1_txt.py
@@ -0,0 +1,23 @@
+
+
+import glob
+
+import pandas as pd
+
+pd.set_option('expand_frame_Repr',False)
+
+path = r'C:\Users\Mat\Desktop\Learning_Python\forextest1.txt' #Location of Forex_1 file
+allfiles = glob.glob(path)  #Loads all the text files onto my dataframe.
+
+frame = pd.DataFrame()  #Frame of dataframe
+
+list_=[]
+
+for file_ in allfiles:
+    df = pd.read_csv(file_,index_col=None,sep='~',header=0)
+    list_.append(df) #Tells python how to read my files in the dataframe
+    
+frame = pd.concat(list_)
+
+frame
+    
